d9ca73df288641a2d1d95445cb4514f34c92bb34
施华军 <shihuajunze@163.com>
2019-05-07 10:55:04 +0800

    fix

---
+++ template/src/routers/home/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Index extends Component {
+  render () {
+    return (
+      <>
+        <h1>Hello BV!</h1>
+        <img src="images/logo.png" width="120" height="50" />
+      </>
+    )
+  }
+}
